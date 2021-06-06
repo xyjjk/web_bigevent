e4c4849d47907c631395796af8e17e1cf98ae3ae
itheima <364637713@qq.com>
2021-06-06 13:16:36 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,4 +3,17 @@
 $.ajaxPrefilter(function(options) {
     // console.log(options.url)
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+    if (options.url.indexOf('/my/') !== -1)
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    options.complete = function(res) {
+
+        // console.log('执行了回调函数');
+        // console.log(res);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            localStorage.removeItem('token')
+            location.href = '/login.html'
+        }
+    }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,42 @@
+$(function() {
+        getUserInfo()
+        var layer = layui.layer
+        $('#btnLogout').on('click', function() {
+            // 提示用户是否确认退出
+            layer.confirm('确定退出登录?', { icon: 3, title: '提示' },
+                function(index) {
+                    localStorage.removeItem('token')
+                    location.href = '/login.html'
+                    layer.close(index)
+                });
+        })
+    })
+    // 获取用户的基本信息
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || ''
+        // },
+        success: function(res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败! ')
+            }
+            renderAvatar(res.data)
+        }
+    })
+}
+
+function renderAvatar(user) {
+    var name = user.nickname || user.username
+    $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
+    if (user.user_pic !== null) {
+        $('.layui-nav-img').attr('src', user.user_pic).show()
+        $('.text-avatar').hide()
+    } else {
+        $('.layui-nav-img').hide()
+        var first = name[0].toUpperCase()
+        $('.text-avatar').html(first).show()
+    }
+}
